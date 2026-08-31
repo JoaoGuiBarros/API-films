057792e6a03cd7835611c27cea39deb21d4f92ce
João Gui Barros dos Santos <165915051+JoaoGuiBarros@users.noreply.github.com>
2026-08-31 09:11:26 -0300

    feat: cria rota post

--- src/routes/film-routes.js
+++ src/routes/film-routes.js
@@ -1,14 +1,19 @@
 import { Hono } from 'hono';
 import { FilmRepository } from '../repositories/film-repository.js';
 import { ListFilmsUseCase } from '../usecases/list-films-usecase.js';
+import { CreateFilmUseCase } from '../usecases/create-film-usecase.js';
 import { FilmController } from '../controllers/film-controller.js';
 
 const filmRoutes = new Hono();
 
 const filmRepository = new FilmRepository();
 const listFilmsUseCase = new ListFilmsUseCase(filmRepository);
-const filmController = new FilmController(listFilmsUseCase);
+const createFilmUseCase = new CreateFilmUseCase(filmRepository);
+
+const filmController = new FilmController(listFilmsUseCase, CreateFilmUseCase);
 
 filmRoutes.get('/filmes', (c) => filmController.list(c));
+
+filmRoutes.post('/filmes', (c) => filmController.create(c));
 
 export { filmRoutes };
